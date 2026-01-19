eaa429a175a962ac01742ed374fb531796b35774
AirZoma <127742515+AirZoma@users.noreply.github.com>
2026-01-19 14:47:34 +0100

    PIAIIIIIIIRE REMET LES POINT ROUGE EN PETIT, L'UTF 8, LES EMOJIES, LE MODE SOMBRE,

--- front-end/src/components/pages/PlayersPage/PlayersPage.js
+++ front-end/src/components/pages/PlayersPage/PlayersPage.js
@@ -1,14 +1,12 @@
 import React, { useRef, useState, Suspense, lazy, forwardRef, useImperativeHandle } from 'react';
 import './PlayersPage.css';
 
-// Lazy load du Globe - ne charge que quand nécessaire
 const Globe = lazy(() => import('../../common/Globe/Globe'));
 
 const PlayersPage = forwardRef(({ user, onLogout, onLoginClick, onDashboardClick, onCitySelect }, ref) => {
   const globeRef = useRef(null);
   const [selectedCity, setSelectedCity] = useState(null);
 
-  // Exposer les méthodes du Globe au parent via ref
   useImperativeHandle(ref, () => ({
     zoomToCity: (cityName) => {
       if (globeRef.current && globeRef.current.zoomToCity) {
