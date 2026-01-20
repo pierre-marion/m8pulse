1372b483979ade1b84a429ced9aeb86db107b036
stonefaII <pierremarion415@gmail.com>
2026-01-20 09:14:03 +0100

    tiens

--- back-end/src/Controller/DatasetController.php
+++ back-end/src/Controller/DatasetController.php
@@ -268,7 +268,7 @@ class DatasetController extends AbstractController
             $dataset->setDescription($data['description'] ?? null);
             $dataset->setFilename($filename);
             $dataset->setSource('Google Sheets: ' . $data['spreadsheet_id']);
-            $dataset->setUploadedBy($this->getUser());
+            $dataset->setUploader($this->getUser()); // Changé: setUploadedBy -> setUploader
             $dataset->setVariables($importedData['variables']); // Auto-détection des types
             
             $this->entityManager->persist($dataset);
